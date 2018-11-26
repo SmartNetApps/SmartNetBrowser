457d2b55f061bb8eddb86f88b0abdfc345dbcdb5
Quentin Pugeat <quentin.pugeat@hotmail.fr>
2018-11-26 11:51:51 +0100

    Suppression de WebpageCollection

--- AFTER-G SmartNet Browser/My Project/AssemblyInfo.vb
+++ AFTER-G SmartNet Browser/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.12.1.0")>
-<Assembly: AssemblyFileVersion("0.12.1.0")>
+<Assembly: AssemblyVersion("0.13.0.0")>
+<Assembly: AssemblyFileVersion("0.13.0.0")>
 <Assembly: NeutralResourcesLanguage("fr")>
