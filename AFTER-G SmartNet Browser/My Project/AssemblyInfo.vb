bf1d7a23112ef2bf200c502a59c014c22f419794
Quentin Pugeat <quentin.pugeat@hotmail.fr>
2020-03-06 13:11:34 +0100

    Changement du numéro de version

--- AFTER-G SmartNet Browser/My Project/AssemblyInfo.vb
+++ AFTER-G SmartNet Browser/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.15.0.0")>
-<Assembly: AssemblyFileVersion("0.15.0.0")>
+<Assembly: AssemblyVersion("0.15.1.0")>
+<Assembly: AssemblyFileVersion("0.15.1.0")>
 <Assembly: NeutralResourcesLanguage("fr")>
